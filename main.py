abce025f0f2a63218a928cb8f6a7c3cf9e8625bd
ttabor16 <ttabor16@gmail.com>
2025-10-26 10:23:20 -1000

    Screen w/ Background implemented

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    #print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+    
 
 
 if __name__ == "__main__":
