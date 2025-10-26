2a7f1cfb5c2e4ea6a46fb5ddcb8a1d717f33e875
ttabor16 <ttabor16@gmail.com>
2025-10-26 10:31:48 -1000

    implement 60 fps

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ import pygame
 from constants import *
 
 def main():
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
 
-
-    
+        # limits framerate to 60 fps
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
