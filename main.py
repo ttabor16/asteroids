7f19f0e4479b5ed98d75d7aad7a756eda625fdd8
ttabor16 <ttabor16@gmail.com>
2025-10-27 16:37:04 -1000

    Player icon and movement implemented

--- main.py
+++ main.py
@@ -3,19 +3,24 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limits framerate to 60 fps
